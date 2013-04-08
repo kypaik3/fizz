a9533c24daf9a1f4ac02632d94bf08bf807979fe
kypaik3 <qorrbduf@hotmail.com>
2013-04-07 21:32:37 -0700

    basic line drawing via two touch. bugs exist

--- Main.lua
+++ Main.lua
@@ -4,21 +4,29 @@ function setup()
     print("This example tracks multiple touches and colors them based on their ID")
     -- keep track of our touches in this table
     touches = {}
+    x = 1
     
 end
 
 function touched(touch)
     if touch.state == ENDED then
-        touches[touch.id] = nil
-    else
-        touches[touch.id] = touch
+        touches[x] = nil
+        x= x-1
+    elseif touch.state == BEGAN then
+        touches[x] = touch
+        x=x +1
     end
+   -- print(x)
+  --  print(#touches)
 end
 
 -- This function gets called once every frame
 function draw()
-    background(0, 0, 0, 255)
-    
+    background(255, 255, 255, 255)
+    a = 0
+    b=0
+    c=0
+    d=0
     for k,touch in pairs(touches) do
         -- Use the touch id as the random seed
         math.randomseed(touch.id)
@@ -26,5 +34,28 @@ function draw()
         fill(math.random(255),math.random(255),math.random(255))
         -- Draw ellipse at touch position
         ellipse(touch.x, touch.y, 100, 100)
-    end
+       -- print(#touches)
+        -- print(k)
+        
+            if k==1 then
+               a = touch.x
+               b = touch.y
+             --  print("accessed k 1")
+            end
+            
+            if k==2 then
+               c = touch.x
+               d = touch.y
+            --     print("accessed k 2")
+            end
+            
+        
+       end 
+       if x>=2 then
+            stroke(0, 0, 0, 255)
+            strokeWidth(50)
+           line(a,b,c,d)
+        end
+            
+    
 end
